38162af4bc7d4f43037eee7d009ffacc2dfbf788
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2022-09-26 22:57:08 -0400

    Update About.vue

--- test_site/vue_app/templates/vue_app/About.vue
+++ test_site/vue_app/templates/vue_app/About.vue
@@ -31,28 +31,18 @@
     </style>
 
     <div class="box">
-      <h1 class="title">About BetterShot</h1>
-    </div>
-
-    <div class="box">
-      <figure class="image is-128x128">
-        <img src="https://cdn.discordapp.com/attachments/930636851667140708/930637064666505256/unknown.png">
-      </figure>
+      <h1 class="title">About Me!</h1>
     </div>
 
     <div class="box">
       <p> 
-        There are a lot of tools that allow users to seamlessly screenshot/record their screens and share that content with their peers such as Lightshot, Gyazo, and ShareX; 
-        however, these tools can sometimes be complicated to set up with a custom domain (ShareX), the software simply can't record your screen (Lightshot), 
-        or the snipping abilities are limited (Gyazo). Of course, there'll never be one single application that checks off all of the boxes, but because this project is FOSS, 
-        users themselves can add their desired features at any point in time-- and even bring it up to the community so it's pushed to everyone's devices. 
-        This project aims to be an open-source version of ShareX, Lightshot, Gyazo, Streamable, and NVIDIA's GeForce Experience screen capturing tool.
+        Hello, this is a test!
       </p>
     </div>
 
     <div class="box">
       <p>
-        This project is led by RPI students Nishant Srivastava, along with members Vincent Chen and Maxwell Tang. BetterShot is an RCOS project.
+        This project is led by Maxwell Tang. He is the coolest!
       </p>
     </div>
 
